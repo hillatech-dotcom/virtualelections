741afd712e1c40aaa1b05688de5efc07e7832c1e
hillatech-dotcom <115955362+hillatech-dotcom@users.noreply.github.com>
2022-10-31 01:49:44 +0300

    my app first commit

--- evoting/settings.py
+++ evoting/settings.py
@@ -69,6 +69,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'whitenoise.middleware.WhiteNoiseMiddleware',
 
                 
             ],
@@ -132,6 +133,7 @@ MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static')
 ]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
